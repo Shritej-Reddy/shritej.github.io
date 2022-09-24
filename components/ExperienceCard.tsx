f9d947898d34a2c8af9d3c4d6cc0ec67d39ea0ea
Shritej-Reddy <tejureddy47@gmail.com>
2022-09-25 01:32:01 +0530

    first commit

---
+++ components/ExperienceCard.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { motion } from "framer-motion";
+import { Experience } from '../typings';
+import { urlFor } from '../sanity';
+
+type Props = {
+    experience: Experience;
+}
+
+const ExperienceCard = ({ experience }: Props) => {
+    console.log("Experience: ", experience);
+  return (
+    <article className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden">
+        <motion.img
+            initial={{ opacity: 0, y: -100 }}
+            transition={{ duration: 1.2, }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true, }}
+            className="w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center"
+            src={urlFor(experience?.companyImage).url()}
+            alt=""
+        />
+
+        <div className="px-0 md:px-10">
+            <h4 className="text-4xl font-light">Full Stack Developer</h4>
+            <p className="font-bold text-2xl mt-1">Paragraph</p>
+            <div className="flex space-x-2 my-2">
+                {experience.technologies.map((technology) => (
+                    <img 
+                    key={technology._id}
+                    className="h-10 w-10 rounded-full" 
+                    src={urlFor(technology.image).url()} 
+                    alt=""/>
+                ))}
+            </div>
+            <p className="uppercase py-5 text-gray-300">
+                {new Date(experience.dateStarted).toDateString()} - {experience.isCurrentlyWorkingHere ? "Present" : new Date(experience.dateEnded).toDateString()}
+            </p>
+            <ul className="list-disc space-y-4 ml-5 text-lg pr-5 max-h-20 w-4/5 overflow-y-scroll scrollbar-thin scrollbar-track-black scrollbar-thumb-[#F7AB0A]/80">
+                {experience.points.map((point, i) => (
+                    <li key={i}>{point}</li>
+                ))}
+            </ul>
+        </div>
+    </article>
+  )
+}
+
+export default ExperienceCard
---
+++ components/Header.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
+import Link from 'next/link';
+import { Social } from '../typings';
+
+type Props = {
+    socials: Social[]
+}
+
+export default function Header({ socials}: Props) {
+    console.log("socialssedjd : ", socials);
+  return (
+    <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
+        <motion.div
+            initial={{
+                x: 500,
+                opacity: 0,
+                scale: 0.5,
+            }}
+            animate={{
+                x: 0,
+                opacity: 1,
+                scale: 1,
+            }}
+            transition={{
+                duration: 1.5,
+            }}
+            className="flex flex-row items-center"
+        >
+            <SocialIcon key={socials[0]._id} url={socials[0].url} fgColor="gray" bgColor="transparent" />
+            <SocialIcon key={socials[1]._id} url={socials[1].url} fgColor="gray" bgColor="transparent" />
+        </motion.div>
+
+        <Link href="#contactMe">
+            <motion.div
+            initial={{
+                x: -500,
+                opacity: 0,
+                scale: 0.5,
+            }}
+            animate={{
+                x: 0,
+                opacity: 1,
+                scale: 1,
+            }}
+            transition={{
+                duration: 1.5,
+            }}
+                className="flex flex-row items-center text-gray-300 cursor-pointer"
+            >
+                <SocialIcon className="cursor-pointer" network="email" fgColor="gray" bgColor="transparent" />
+                <p className="uppercase hidden md:inline-flex text-sm text-gray-400">Get in touch</p>
+            </motion.div>
+        </Link>
+    </header>
+  )
+}
